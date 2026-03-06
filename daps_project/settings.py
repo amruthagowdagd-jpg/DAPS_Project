3df80baceb23b437a79fa94e58fbd69f1b43f767
amrutha <amruthagowdagd@gmail.com>
2026-03-06 11:53:49 +0530

    Deployment set

--- daps_project/settings.py
+++ daps_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f8r77u$3=chzo-+6+puv2#j73$@itv-b255bf2n94x1_y4(##7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
+DEBUG = os.environ.get("DEBUG") == "True"
+
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
